d93f7b0bfef7866711a1b41bedcfb20de4be5239
SouthernHU <75197068+SouthernHU@users.noreply.github.com>
2024-12-23 09:35:40 +0800

    Update script_clashVerge_rulesProvider.js
    原先的自定义规则移入overseaLite规则组中

--- domainFormat/script_clashVerge_rulesProvider.js
+++ domainFormat/script_clashVerge_rulesProvider.js
@@ -106,13 +106,6 @@ const ruleProviders = {
 };
 // 规则
 const rules = [
-  // 自定义规则
-  "DOMAIN-SUFFIX,googleapis.cn,节点选择", // Google服务
-  "DOMAIN-SUFFIX,googleapis.com,节点选择", // Google服务
-  "DOMAIN-SUFFIX,gstatic.com,节点选择", // Google静态资源
-  "DOMAIN-SUFFIX,xn--ngstr-lra8j.com,节点选择", // Google Play下载服务
-  "DOMAIN-SUFFIX,github.io,节点选择", // Github Pages
-  "DOMAIN,v2rayse.com,节点选择", // V2rayse节点工具
   // 其他规则
   "GEOIP,LAN,DIRECT,no-resolve",
   "GEOIP,CN,DIRECT,no-resolve",
